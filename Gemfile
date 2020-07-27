20a92dee40e53ede16fc22ef75afb3970b97d4b3
xdiogoslipx <diogoslip39@gmail.com>
2020-07-27 10:55:36 -0300

    Revert "Fleury - Automação Web"

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "http://rubygems.org"
-
-gem "capybara", "3.18.0"
-gem "cucumber", "3.1.2"
-gem "rspec", "3.8.0"
-gem "selenium-webdriver", "3.142.0"
--- features/step_definitions/pagina_home.rb
+++
@@ -1,9 +0,0 @@
-Dado('que estou na página Home') do
-    @home.go
-end
-  
-Quando('acesso o menu Unidades') do
-   @home.link_unidades
-   expect(page).to have_content('Você está em: ')
-end
- 
